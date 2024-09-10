6a48825a69b5f746fc35f2f343d54f754d067663
Aarav Goel <73917648+aaravgoel5@users.noreply.github.com>
2024-09-09 17:52:11 -0700

    Update turtlerunner.py

--- turtlerunner.py
+++ turtlerunner.py
@@ -6,16 +6,23 @@ screen.setup(width=800, height=800)
 trtl = trtl.Turtle()
 trtl.speed(0)
 
-size = 20
-size_increment = 20
+colors = ['red', 'green', 'blue', 'black', 'orange', 'purple', 'pink', 'brown']
 
-while True:
+size = 20
+size_incrm = 20
+incrm = 0
+
+while True:  
     trtl.penup()
     trtl.goto(-size / 2, -size / 2)
     trtl.pendown()
-    for _ in range(4):
+    trtl.color(colors[incrm % len(colors)]) 
+    j = 0
+    while j < 4:  
         trtl.forward(size)
         trtl.left(90)
-    size += size_increment
-  
+        j += 1
+    size += size_incrm 
+    incrm += 1
+    
 screen.mainloop()
